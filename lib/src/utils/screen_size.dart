c710152b2b49edf1dd2a148b0a6c887885664729
Maksim Dyachkov <maksimdyachkov1995@gmail.com>
2020-08-11 15:23:21 +0300

    wprking all day

--- lib/src/utils/screen_size.dart
+++ lib/src/utils/screen_size.dart
@@ -5,3 +5,5 @@ const double baseHeight = 650.0;
 double screenAwareSize(double size, BuildContext context) {
   return size * MediaQuery.of(context).size.height / baseHeight;
 }
+
+// AAAAAAAAAAAAAAAAAAAAgit
